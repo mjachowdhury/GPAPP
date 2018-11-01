a83df06fb74e9006e9121160523daa43613def06
mjachowdhury <study.computing2016@gmail.com>
2018-11-01 15:06:52 +0000

    Added group project first commit

---
+++ app/src/main/java/com/mohammed/transport/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.mohammed.transport;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AbsHQFDncI_cAdy1dTtBXSB2znBJi3Y-BxXoLhDrtuQiiKju_6j_Lk4W6wHjSZ1syn1wy8Rwsh8w_IkZ";
+}
