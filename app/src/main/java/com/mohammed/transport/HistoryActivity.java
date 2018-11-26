27dbbaa13c7443908cf94d6365e680da91fe04d4
mjachowdhury <study.computing2016@gmail.com>
2018-11-26 18:56:34 +0000

    modified the code for the newWorkingBranch

--- app/src/main/java/com/mohammed/transport/HistoryActivity.java
+++ app/src/main/java/com/mohammed/transport/HistoryActivity.java
@@ -222,3 +222,94 @@ public class HistoryActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
+//===========================================
+
+/*
+public class HistoryActivity extends AppCompatActivity {
+
+    private RecyclerView mHistoryRecyclerView;
+    private RecyclerView.Adapter mHistoryAdapter;
+    private RecyclerView.LayoutManager mHistoryLayoutManager;
+    private String customerOrDriver, userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        mHistoryRecyclerView = (RecyclerView) findViewById(R.id.historyRecyclerView);
+        mHistoryRecyclerView.setNestedScrollingEnabled(false);
+        mHistoryRecyclerView.setHasFixedSize(true);
+
+        mHistoryLayoutManager = new LinearLayoutManager(HistoryActivity.this);
+        mHistoryRecyclerView.setLayoutManager(mHistoryLayoutManager);
+        mHistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
+        mHistoryRecyclerView.setAdapter(mHistoryAdapter);
+
+        customerOrDriver = getIntent().getExtras().getString("customerOrDriver");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getUserHistoryIds();
+    }
+
+    private void getUserHistoryIds() {
+        DatabaseReference userHistoryDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(customerOrDriver).child(userId).child("history");
+        userHistoryDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot history : dataSnapshot.getChildren()) {
+                        fetchRideInformation(history.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void fetchRideInformation(String rideKey) {
+        DatabaseReference historyDatabase = FirebaseDatabase.getInstance().getReference().child("History").child(rideKey);
+        historyDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String rideId = dataSnapshot.getKey();
+                    Long timestamp = 0L;
+                    for(DataSnapshot child : dataSnapshot.getChildren()) {
+                        if (child.getKey().equals("timestamp")) {
+                            timestamp = Long.valueOf(child.getValue().toString());
+                        }
+                    }
+                    HistoryObject obj = new HistoryObject(rideId, getDate(timestamp));
+                    resultsHistory.add(obj);
+                    mHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String getDate(Long timestamp) {
+        Calendar cal = Calendar.getInstance(Locale.getDefault());
+        cal.setTimeInMillis(timestamp * 1000);
+        String date = DateFormat.format("MM-dd-yyyy hh:mm", cal).toString();
+        return date;
+    }
+
+    private ArrayList resultsHistory = new ArrayList<HistoryObject>();
+    private ArrayList<HistoryObject> getDataSetHistory() {
+        return resultsHistory;
+    }
+}
+*/
+
